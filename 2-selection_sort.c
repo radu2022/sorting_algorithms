5bbc4969e2afff305f6147b6992c643949affe0a
radu2022 <seid.hamid2@gmail.com>
2022-11-04 06:46:06 -0700

    bubble sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,41 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array following the selection sort algorithm
- * @array: array to sort
- * @size: size of the array;
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
-  unsigned int i, j, min;
-
-  register int tmp;
-
-  if (size < 2)
-    return;
-
-  for (i = 0; i < size; i++)
-    {
-      min = i;
-      for (j = i + 1; j < size; j++)
-	{
-	  if (array[j] < array[min])
-	    min = j;
-	}
-      tmp = array[i];
-      array[i] = array[min];
-      array[min] = tmp;
-      if (i != min)
-	print_array(array, size);
-    }
+int *min;
+size_t i, j;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+min = array + i;
+for (j = i + 1; j < size; j++)
+min = (array[j] < *min) ? (array + j) : min;
+if ((array + i) != min)
+{
+swap_ints(array + i, min);
+print_array(array, size);
+}
+}
 }
